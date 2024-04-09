d8444a5120947195c2f7f4b9496a70c567b76358
stephy <stephshiro35@gmail.com>
2024-04-09 09:57:25 +0300

    add

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 
-const input = document.getElementById('button');
 
-function clickAlert() {
-  alert('I was clicked!');
+// index.js
+
+function addingEventListener() {
+  const input = document.getElementById('button');
+  input.addEventListener('click', function() {
+      alert('I was clicked!');
+  });
 }
 
-input.addEventListener('click', clickAlert);
+addingEventListener(); // Call the function to set up the event listener
